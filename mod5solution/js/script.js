230c6c64d6bd20a18e3de7ec57e5cc8a1103ce25
sgwiseco <kungkwei@gmail.com>
2016-08-18 13:53:42 +0800

    Update script.js

--- mod5solution/js/script.js
+++ mod5solution/js/script.js
@@ -118,10 +118,9 @@ function buildAndShowHomeHTML (categories) {
       // 
       // var homeHtmlToInsertIntoMainPage = ....
       
-      chosenCategoryShortName1 = "'" + chosenCategoryShortName + "'";
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 
-                                        "randomCategoryShortName",
-                                        chosenCategoryShortName1);
+                                        "randomCategoryShortName", chosenCategoryShortName);
   
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
